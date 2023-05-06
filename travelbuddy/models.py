daa66605ac982b4ece17d93950a22c3fa4d6afd4
soroblos <sorochukwuoputa14@gmail.com>
2023-05-05 22:02:15 -0400

    changing model charfield

--- travelbuddy/models.py
+++ travelbuddy/models.py
@@ -4,7 +4,7 @@ class TravelBuddy(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
-    img = models.CharField(max_length=100)
+    img = models.CharField(max_length=1000)
     upvote = models.IntegerField()
     downvote = models.IntegerField()
 
